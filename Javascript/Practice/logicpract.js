8588ca0d8cb2e5694d59a14f7987a34a19b9178f
Sunil Bhave <sunilbhave69@gmail.com>
2021-12-28 23:32:05 +0530

    commit

---
+++ Javascript/Practice/logicpract.js
@@ -0,0 +1,70 @@
+// print number 1 to 10
+
+// for(i = 0; i<=10; i++){
+//     console.log(i);
+
+// }
+
+//**------------------- */
+
+//Program to calculate first 10 natural number
+
+// let sum = 0;
+// for (let i = 1; i <=10; i++) {
+//     sum += i;
+    
+// }
+// console.log(sum)
+
+/*----------------**----------------*/
+
+// mult table by prompt
+
+// let num = prompt("enter the number for multiplication");
+// for(i = 1; i<=10; i++){
+//     console.log(num + "*" + i+ "=" + num*i);
+// }
+/*----------------**----------------*/
+
+// program to find factorial
+// let x = prompt("enter the number to find factorial");
+// let fact = 1;
+// for(i = 1; i <=x; i++){
+//     fact *=i;
+//     console.log(fact);
+// }
+/*----------------**----------------*/
+
+// program to find the power with inputs
+
+// let x = prompt("enter 1st number");
+// let y = prompt("enter 2nd number");
+// let res = 1;
+
+// for(i=1; i<=y; i++){
+    //     res *= x;
+    // }
+    // console.log(res);
+    /*----------------**----------------*/
+    
+    // reverse the input digit;
+    
+    // let num = prompt("enter the number");
+    // let rev = 0;
+    // let temp = num;
+    // let rem = 0;
+    // while(temp>0){
+        
+    //     rem = temp %10;
+    //     rev = rev * 10 + rem;
+    //     temp = Math.trunc(temp/10);
+        
+        
+    // }
+    // console.log(rev);
+    
+    /*----------------**----------------*/
+
+
+
+
---
+++ Python/28-12PY.py
@@ -0,0 +1,7 @@
+print("Name: Sunil")
+print("class")
+print("Sunil Bhave")
+print(4 + 5)
+print(4*4)
+print("4 + 5 ")
+print(10.07 + 11.06)
