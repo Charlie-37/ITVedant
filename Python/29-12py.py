19d144d2edef2342409bc219a3a9c203bc551585
Sunil Bhave <sunilbhave69@gmail.com>
2021-12-29 20:13:55 +0530

    commit

---
+++ Python/29-12py.py
@@ -0,0 +1,58 @@
+
+# /------------User Defined input function-------------/
+# x = input("enter the 1st value")
+# x = int(x)
+# y = input("enter the 2nd number")
+# y = int(y)
+# sum = x + y
+# print(sum)
+
+# x = int(input("enter 1st number "))
+# y = int(input("enter 2nd number "))
+# sum = x + y
+# print(sum)
+
+# /------------Name Address using User defined------------*/
+
+# Fname = input("enter you Full Name ")
+# city = input("Enter your city ")
+# print("Full Name : ", Fname)
+# print("City : ", city)
+
+
+
+# /------------Area of Circle using User defined------------*/
+
+# pi = float(input("Enter the value of Pi : "))
+# r = float(input("Enter the Radius of Circle : "))
+# CircArea = pi * r * r
+# print("Area of Circle is : ", CircArea)
+
+
+
+# /------------Area of Square using User defined------------*/
+
+# side = int(input("Enter the side of the Square : "))
+# AreaSq = side * side
+# print("Area of the Square is : ", AreaSq)
+
+
+
+# /------------Area of reactangle using User defined------------*/
+
+# length = float(input("Enter the lenght of Reactagle : "))
+# breath = float(input("Enter the breath of Reactagle : "))
+# AreaRect = length * breath
+# print("Area of the Reactangle is : ", AreaRect)
+
+
+
+# /------------Cube of given number using User defined------------*/
+
+# cubnum = int(input("Enter the Number : "))
+# resCube = cubnum * cubnum * cubnum
+# print("Cube of the given Number is : ", resCube)
+
+
+
+
