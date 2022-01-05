9f3f24882ec0bcb1a177ecd7b8824fd464591e1b
Sunil Bhave <75983741+Charlie-37@users.noreply.github.com>
2022-01-05 15:11:50 +0530

    enter

--- Python/28-12(2)PY.py
+++ Python/28-12(2)PY.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #WAP to add 2 num
 num1 = 10
 num2 = 30
@@ -35,3 +36,42 @@ result  = length * width
 print("Area of rectangle is:", result)
 
 
+=======
+#WAP to add 2 num
+num1 = 10
+num2 = 30
+resnum = num1 + num2
+print("Addition of two No. is :",resnum)
+
+#WAP To find area of circle
+
+pi = 3.141
+r = 11
+area = pi * r * r
+print( "Area of Circle is :",area)
+
+
+#WAP to find area of Square
+
+side = 6
+ressq = side  * side
+print("Side of square is :",ressq)
+
+#WAP cube of number
+
+n1 = 20
+resCub = n1 *n1 * n1
+print("Cube of the number is :", resCub)
+
+
+#Area of rectangle by user input
+
+length = input("1st number ")
+length = int(length)
+width = input("2nd number ")
+width = int(width)
+result  = length * width
+print("Area of rectangle is:", result)
+
+
+>>>>>>> 8588ca0 (commit)
