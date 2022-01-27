045aa8ccb833a318124dfdbca9c7921caead7429
Sunil Bhave <75983741+Charlie-37@users.noreply.github.com>
2022-01-27 15:35:35 +0530

    added task

---
+++ Python/Task/25-01Task.py
@@ -0,0 +1,15 @@
+# //*-------------Addition of two User inputs----------------*//
+
+# x = int(input("Enter First Number : "))
+# y = int(input("Enter Second Number : "))
+# z = x + y
+# print("Addition of : ", x, "+", y, "=" , z)
+
+
+# //*-------------Simple Interest----------------*//
+
+# pi = int(input("Enter Principle Amount : "))
+# r = int(input("Enter The Interset Rate : "))
+# t = int(input("Enter The Duration in Years: "))
+# Si = (pi * r * t)/100
+# print("Simple Interset is : ", Si)
