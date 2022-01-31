94c449a4e3b8bc58fb33607ed0db8d7c200a57da
Sunil Bhave <75983741+Charlie-37@users.noreply.github.com>
2022-01-31 20:53:34 +0530

    changes

--- Python/31-01Text1.py
+++ Python/31-01Text1.py
@@ -1,34 +1,70 @@
-x = float(input("Enter the First Number : "))
-y = float(input("Enter the Second Number : "))
-opert = input("Enter the operator : ")
+# x = float(input("Enter the First Number : "))
+# y = float(input("Enter the Second Number : "))
+# opert = input("Enter the operator : ")
 
-if opert == "+":
-    z = x+y
-    print(x, opert, y, "=", z)
+# if opert == "+":
+#     z = x+y
+#     print(x, opert, y, "=", z)
 
-elif opert == "-":
-    z = x - y
-    print(x, opert, y, "=", z)
+# elif opert == "-":
+#     z = x - y
+#     print(x, opert, y, "=", z)
 
-elif opert == "/":
-    z = x / y
-    print(x, opert, y, "=", z)
+# elif opert == "/":
+#     z = x / y
+#     print(x, opert, y, "=", z)
 
-elif opert == "*":
-    z = x * y
-    print(x, opert, y, "=", z)
+# elif opert == "*":
+#     z = x * y
+#     print(x, opert, y, "=", z)
 
-elif opert == "%":
-    z = x % y
-    print(x, opert, y, "=", z)
+# elif opert == "%":
+#     z = x % y
+#     print(x, opert, y, "=", z)
 
-elif opert == "//":
-    z = x // y
-    print(x, opert, y, "=", z)
+# elif opert == "//":
+#     z = x // y
+#     print(x, opert, y, "=", z)
 
-elif opert == "**":
-    z = x ** y
-    print(x, opert, y, "=", z)
+# elif opert == "**":
+#     z = x ** y
+#     print(x, opert, y, "=", z)
 
-else :
-    print("Invalid Input")
+# else :
+#     print("Invalid Input")
+
+
+#//*------------------**----------------//
+
+# x = float(input("Enter the First Number(X) : "))
+# y = float(input("Enter the Second Number(Y) : "))
+
+# if x < y:
+#     print(y, "is Greater")
+
+# elif x > y:
+#     print(x, "is Greater")
+
+# elif x == y:
+#     print("Both are equal")
+
+
+#//*------------------**----------------//
+
+m = float(input("Enter the Marks : "))
+
+if m >= 75 and m <=100:
+    print("Grade is A")
+
+elif m>=60 and m<75:
+    print("Grade is B")
+
+elif m>=35 and m<60:
+    print("Grade is c")
+
+elif m<35 :
+    print("Fail")
+
+else:
+    print("Invalid marks")
+
