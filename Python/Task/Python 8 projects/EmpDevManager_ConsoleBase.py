ea3cc14be76cce5adff9ccd90507e2c3357f0fd5
Sunil Bhave <75983741+Charlie-37@users.noreply.github.com>
2022-04-10 16:25:24 +0530

    python proj

--- Python/Task/Python 8 projects/EmpDevManager_ConsoleBase.py
+++ Python/Task/Python 8 projects/EmpDevManager_ConsoleBase.py
@@ -1,5 +1,8 @@
 
+import pymysql as p
 
+def dbConnect():
+    return p.connect(host="Localhost", user="root", password="", database="empdevman")
 # //*--- Employee Manager Develpoer Class-----*//
 
 class Employees():
@@ -12,6 +15,9 @@ class Employees():
         self.salary = salary
         self.email = f"{self.first.lower()}.{self.last.lower()}@itvedant.com"
         Employees.empcount+=1
+
+    def pushDet(self):
+        return self.first, self.last, self.salary, self.email 
 
     def increment(self):
         self.salary = self.salary * self.incamt
@@ -50,38 +56,130 @@ class Manager(Employees):
             print(f"----> {emp.fullname()}")
        
 
-emp1 = Employees("sunil", "bhave", 25000)
-emp2 = Employees("bhavesh", "ainkar", 2200)
-emp3 = Employees("Rutuja", "sawant", 10000)
-emp4 = Employees("Mahesh", "bhat", 15000)
-emp5 = Employees("suraj", "surwanshi", 45000)
-emp6 = Employees("Tejas", "Jadhav", 75000)
+##emp1 = Employees("sunil", "bhave", 25000)
+##emp2 = Employees("bhavesh", "ainkar", 2200)
+##emp3 = Employees("Rutuja", "sawant", 10000)
+##emp4 = Employees("Mahesh", "bhat", 15000)
+##emp5 = Employees("suraj", "surwanshi", 45000)
+##emp6 = Employees("Tejas", "Jadhav", 75000)
+##
+##print(emp6.increment())
+##
+##dev1 = Developer("Dipali", "Bhangrath", 300000,"Python")
+##dev2 = Developer("Ruchita", "Gurav", 400000,"Java")
+##
+##print(dev2.fullname())
+##print(dev2.email)
+##print(dev2.increment())
+##
+##
+##
+##manag1 = Manager("Sapnil", "rajesh", 45222)
+##manag2 = Manager("Rahul", "kanojiya", 65222)
+##
+##manag2.addemp(emp2)
+##manag2.addemp(emp1)
+##manag2.addemp(emp5) 
+##manag2.addemp(dev2) 
+##
+##print(manag2.printemp())
+##
+##manag2.removeemp(emp5)
+##print(manag2.printemp())
+##print(manag2.fullname())
+##print(manag2.increment())
 
-print(emp6.increment())
 
-dev1 = Developer("Dipali", "Bhangrath", 300000,"Python")
-dev2 = Developer("Ruchita", "Gurav", 400000,"Java")
+def empdev():
+    while True:
+        print("\n")
+        print("Enter 1 to Add Employee")
+        print("Enter 2 to Add Developer")
+        print("Enter 3 to Add Manager")
+        print("Enter 4 to Show Employee Details")
+        print("Enter 5 to Show Developer Details")
+        print("Enter 6 to Show Manager Details")
+        print("Enter 7 to Show Increment Employee Salary")
+        print("Enter 8 to Show Increment Developer Salary")
+        print("Enter 9 to Show Increment Manager Salary")
+        print("Enter 10 to Add Employee in Manager")
+        print("Enter 11 to Add Developer in Manager")
+        print("\n")
 
-print(dev2.fullname())
-print(dev2.email)
-print(dev2.increment())
+        choi = input("Enter Your Choice : ").strip()
+        print("\n")
+
+        if choi == "1":
+            efirst = input("Enter Employee First Name : ").strip()
+            elast = input("Enter Employee Last Name : ").strip()
+            esal = input("Enter Employee Salary : ").strip()
+            esal = int(esal)
+
+            emp1 = Employees(efirst, elast, esal)
+            x,y,z,w = emp1.pushDet()
+            print(x,y,z,w)
+
+            # pushing to database
+
+            db = dbConnect()
+            cr = db.cursor()
+
+            t = (x,y,z,w)
+            sql = "insert into emp values(%s,%s,%s,%s)"
+
+            cr.execute(sql,t)
+
+            db.commit()
+            db.close()
+
+            
+            
+
+        elif choi == "2":
+            pass
+
+        elif choi == "3":
+            pass
+
+        elif choi == "4":
+            pass
+
+        elif choi == "5":
+            pass
+
+        elif choi == "6":
+            pass
+
+        elif choi == "7":
+            pass
+
+        elif choi == "8":
+            pass
+
+        elif choi == "9":
+            pass
+
+        elif choi == "10":
+            pass
+
+        elif choi == "11":
+            pass
+        
+        
+empdev()    
 
 
 
-manag1 = Manager("Sapnil", "rajesh", 45222)
-manag2 = Manager("Rahul", "kanojiya", 65222)
-
-manag2.addemp(emp2)
-manag2.addemp(emp1)
-manag2.addemp(emp5) 
-manag2.addemp(dev2) 
-
-print(manag2.printemp())
-
-manag2.removeemp(emp5)
-print(manag2.printemp())
-print(manag2.fullname())
-print(manag2.increment())
 
 
 
+
+
+
+
+
+
+
+
+
+        
